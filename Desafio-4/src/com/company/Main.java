8182f1b886361d0a54e2bf7d2f98155c3a6be2d2
Leonel FJR <leoneljararavarotto@hotmail.com>
2021-08-20 01:34:15 -0300

    Trabajo Desafio 4

---
+++ Desafio-4/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nombre;
+        int edad;
+        float salario;
+        boolean carnet = true;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("¿Cual es tu nombre?.");
+        nombre = s.nextLine();
+
+        System.out.println("¿Cual es tu edad?.");
+        edad = s.nextInt();
+
+        System.out.println("¿Cual es el salario que tu deseas?.");
+        salario = s.nextFloat();
+
+        System.out.println("Hola soy " + nombre + " tengo " + edad + ", y me gustaria un salario de " + salario +
+                "$ en el futuro y el carnet es " + carnet);
+    }
+}
