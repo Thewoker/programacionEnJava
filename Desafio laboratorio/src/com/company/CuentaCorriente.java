eb7916980638eada75969569d8a92945f9dc706c
Leonel FJR <leoneljararavarotto@hotmail.com>
2021-09-09 03:34:01 -0300

    Desafio 11

---
+++ Desafio laboratorio/src/com/company/CuentaCorriente.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class CuentaCorriente {
+    private double saldo;
+    private String nombreTitular;
+    private  long numeroCuenta;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getNombreTitular() {
+        return nombreTitular;
+    }
+
+    public void setNombreTitular(String nombreTitular) {
+        this.nombreTitular = nombreTitular;
+    }
+
+    public long getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(long numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+}
