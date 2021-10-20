446c015b2b7913b6e654adc0146e10278e94f711
Leonel FJR <83437197+Thewoker@users.noreply.github.com>
2021-10-20 15:35:58 -0300

    Cine Terminado

--- Cine/src/TGN1/Cine.java
+++ Cine/src/TGN1/Cine.java
@@ -1,8 +1,10 @@
 package TGN1;
 
+import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+import static java.awt.SystemColor.info;
 import static java.lang.Integer.parseInt;
 
 public class Cine {
@@ -28,8 +30,18 @@ public class Cine {
 
     private static Espectadores[] cargarEspectadores(Scanner s, int cantidad) {
         Espectadores[] espectadores = new Espectadores[cantidad];
-        ArrayList <Integer> coleccionEspectadores;
-        coleccionEspectadores = new ArrayList<>();
+        ArrayList <Integer> coleccionEspectadoresA;
+        coleccionEspectadoresA = new ArrayList<>();
+        ArrayList <Integer> coleccionEspectadoresB;
+        coleccionEspectadoresB = new ArrayList<>();
+        ArrayList <Integer> coleccionEspectadoresC;
+        coleccionEspectadoresC = new ArrayList<>();
+        ArrayList <Integer> coleccionEspectadoresD;
+        coleccionEspectadoresD= new ArrayList<>();
+        ArrayList <Integer> coleccionEspectadoresE;
+        coleccionEspectadoresE = new ArrayList<>();
+        ArrayList <Integer> coleccionEspectadoresF;
+        coleccionEspectadoresF = new ArrayList<>();
 
 
         System.out.println("Ingresar los siguientes datos de los espectadores: ");
@@ -40,7 +52,7 @@ public class Cine {
         String fila;
         String sillaString;
         int silla = 0;
-
+        boolean varInfo = false;
 
         for(int i = 0; i < espectadores.length; i++){
 
@@ -48,15 +60,19 @@ public class Cine {
 
             nombre = validacionString("nombre: ", s);
 
-            System.out.print("Edad: ");
-            edadString = s.nextLine();
+            do {
 
-            try {
-                edad = parseInt(edadString);
-            }catch (Exception e){
-                System.out.println("ERROR EN EL INGRESO DE DATOS");
-                System.exit(1); //Finalizar programa
-            }
+                System.out.print("Edad: ");
+                edadString = s.nextLine();
+
+                try {
+                    edad = parseInt(edadString);
+                    varInfo = true;
+                } catch (Exception e) {
+                    System.out.println("ERROR EN EL INGRESO DE DATOS");
+                    varInfo = false;
+                }
+            }while(varInfo == false);
 
             fila = ingresarSoloLetras();
 
@@ -74,17 +90,97 @@ public class Cine {
                     System.out.println("Error. No has ingresado un numero");
                 }
 
-                    if (silla > 0 && silla <= cantidad ) {
-                        if(coleccionEspectadores.contains(silla) == false) {
-                            correcto = true;
-                            auxiliar--;
-                            coleccionEspectadores.add(silla);
-                        }
-                        else System.out.println("Silla no disponible. Intente nuevamente.");
-                    } else {
-                        System.out.println("La silla ingresada no existe");
-                    }
+                //Agregar la fila a la condicion del if de abajo asi tambien tiene en cuenta la fila, y agregar un if por cada fila
+                switch (fila) {
+                    case "a": {
+                        if (silla > 0 && silla <= cantidad) {
+                            if (coleccionEspectadoresA.contains(silla) == false) {
+                                correcto = true;
+                                auxiliar--;
+                                coleccionEspectadoresA.add(silla);
+                            } else System.out.println("Silla no disponible. Intente nuevamente.");
 
+                        } else {
+                            System.out.println("La silla ingresada no existe");
+                        }
+                    }
+                    break;
+
+                    case "b": {
+                        if (silla > 0 && silla <= cantidad) {
+                            if (coleccionEspectadoresB.contains(silla) == false) {
+                                correcto = true;
+                                auxiliar--;
+                                coleccionEspectadoresB.add(silla);
+                            } else System.out.println("Silla no disponible. Intente nuevamente.");
+
+                        } else {
+                            System.out.println("La silla ingresada no existe");
+                        }
+                    }
+                    break;
+
+                    case "c": {
+                        if (silla > 0 && silla <= cantidad) {
+                            if (coleccionEspectadoresC.contains(silla) == false) {
+                                correcto = true;
+                                auxiliar--;
+                                coleccionEspectadoresC.add(silla);
+                            } else System.out.println("Silla no disponible. Intente nuevamente.");
+
+                        } else {
+                            System.out.println("La silla ingresada no existe");
+                        }
+                    }
+                    break;
+
+                    case "d": {
+                        if (silla > 0 && silla <= cantidad) {
+                            if (coleccionEspectadoresD.contains(silla) == false) {
+                                correcto = true;
+                                auxiliar--;
+                                coleccionEspectadoresD.add(silla);
+                            } else System.out.println("Silla no disponible. Intente nuevamente.");
+
+                        } else {
+                            System.out.println("La silla ingresada no existe");
+                        }
+                    }
+                    break;
+
+                    case "e": {
+                        if (silla > 0 && silla <= cantidad) {
+                            if (coleccionEspectadoresE.contains(silla) == false) {
+                                correcto = true;
+                                auxiliar--;
+                                coleccionEspectadoresE.add(silla);
+                            } else System.out.println("Silla no disponible. Intente nuevamente.");
+
+                        } else {
+                            System.out.println("La silla ingresada no existe");
+                        }
+                    }
+                    break;
+
+                    case "f": {
+                        if (silla > 0 && silla <= cantidad) {
+                            if (coleccionEspectadoresF.contains(silla) == false) {
+                                correcto = true;
+                                auxiliar--;
+                                coleccionEspectadoresF.add(silla);
+                            } else System.out.println("Silla no disponible. Intente nuevamente.");
+
+                        } else {
+                            System.out.println("La silla ingresada no existe");
+                        }
+                    }
+                    break;
+
+                    default: {
+                        System.out.println("Error fila ingresada invalida o fuera de los limites.(Ingresa solo minusculas)");
+
+                    }
+                }
             }while (correcto== false);
 
             try {
@@ -120,32 +216,40 @@ public class Cine {
         String letras;
         int cont = 0;
         boolean condicion = false;
-        do{
-            System.out.print("Fila: ");
-            letras = str.nextLine();
+        List<String> varLetras;
+        varLetras = new ArrayList<>();
+        varLetras.add("a");
+        varLetras.add("b");
+        varLetras.add("c");
+        varLetras.add("d");
+        varLetras.add("e");
+        varLetras.add("f");
 
-
-            for (int i = 0; i<letras.length(); i++){
-                if (Character.isLetter(letras.charAt(i))){
-                    cont ++;
-                }
-                else{
-                    cont =2;
-                }
-            }
-
-
-            if (cont > 1){
-                System.out.println("Error solo puede contener  1 letras. Intente de nuevo\n");
-                condicion = false;
+        do {
+            do {
+                System.out.print("Fila (a-f): ");
+                letras = str.nextLine();
                 cont = 0;
-            }
-            else {
-                condicion = true;
-            }
-        }
-        while (condicion == false);
 
+                for (int i = 0; i < letras.length(); i++) {
+                    if (Character.isLetter(letras.charAt(i))) {
+                        cont++;
+                    } else {
+                        cont = 2;
+                    }
+                }
+
+
+                if (cont > 1) {
+                    System.out.println("Error solo puede contener  1 letras. Intente de nuevo\n");
+                    condicion = false;
+                    cont = 0;
+                } else {
+                    condicion = true;
+                }
+            }
+            while (condicion == false);
+        }while(varLetras.contains(letras) == false);
         return letras;
     }
 }
